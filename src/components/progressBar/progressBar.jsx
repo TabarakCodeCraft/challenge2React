d7c93f2aa2bbb2206f97e79b34b283308abeaaeb
TabarakCodeCraft <tabarak.ali1801a@sc.uobaghdad.edu.iq>
2024-05-12 17:01:00 +0300

    update

--- src/components/progressBar/progressBar.jsx
+++ src/components/progressBar/progressBar.jsx
@@ -1,53 +1,26 @@
 import React from "react";
+import './progress.css';
 
 const ProgressBar = (props) => {
     const { title, bgcolor, value } = props;
 
-    const containerStyles = {
-        display: 'flex',
-        justifyContent:"space-between",
-        marginBottom: '10px' ,
-        height: 27,
-        width: '100%',
-        backgroundColor: "#e0e0de",
-        borderRadius: 50,
-    }
-
-    const fillerStyles = {
-        height: '100%',
-        width: `${value}%`,
-        backgroundColor: bgcolor,
-        borderRadius: 'inherit',
-        textAlign: 'right'
-    }
-
-    const labelStyles = {
-        padding: "5px 10px",
-        color: 'white',
-        fontWeight: 'bold'
-    }
-
-    const titleStyle ={
-        fontWeight:"500",
-        margin: "20px 10px 10px 0",
-        color: '#0E183E',
-        fontSize:'16px',
-        fontFamily: 'Poppins',
-    };
-
     const completed = Math.round(value);
 
     return (
-        <div>
-            <h3 style={titleStyle}>{title}</h3>
-            <div style={containerStyles}>
-                <div style={fillerStyles}>
-                    <span style={labelStyles}></span>
-                    <span style={{fontFamily:"Poppins", fontWeight:"500", fontSize:"16px", marginLeft: "10px"}}>{`${completed}%`}</span>
+        <>
+            <h3 className="title">{title}</h3>
+            <div className="myProgress">
+                <div className="container">
+                    <div className="filler" style={{ width: `${value}%`, backgroundColor: bgcolor }}></div>
+                </div>
+
+                <div className="flex">
+                    <span className="label"></span>
+                    <span className="percentage">{`${completed}%`}</span>
+
                 </div>
             </div>
-    
-        </div>
+        </>
     );
 };
 
